0de51482f961d72d94a8f2935f188a9a23faf6e0
robertej19 <robertej@mit.edu>
2020-02-10 13:59:31 -0500

    ""

--- ep_test.groovy
+++ ep_test.groovy
@@ -36,14 +36,58 @@ class ep_test {
 			}
 			println "index_of_electrons_and_protons "+index_of_electrons_and_protons
 
-			def ipi0s = (0..<partb.rows()-1).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
+			def index_of_pions = (0..<partb.rows()-1).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
 		          .findAll{ig1->'xyz'.collect{partb.getFloat("p$it",ig1)**2}.sum()>0.16}
 		          .collectMany{ig1->
 		          (ig1+1..<partb.rows()).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
 		          .findAll{ig2->'xyz'.collect{partb.getFloat("p$it",ig2)**2}.sum()>0.16}
 		          .collect{ig2->[ig1,ig2]}
 		          }
-			println "index of pions is " + ipi0s
+			println "index of pions is " + index_of_pions
+
+			def isep0s = ieps.findAll{iele,ipro->
+			  def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
+			  def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
+
+			  if(event.hasBank("MC::Particle")) {
+			    def mcb = event.getBank("MC::Particle")
+			    def mfac = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 3.2 : 2.5
+
+			    def profac = 0.9
+
+			    //mfac=1
+			    profac = 1.0
+
+			    ele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0) + (partb.getFloat(it,iele)-mcb.getFloat(it,0))*mfac})
+			    pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{profac*(mcb.getFloat(it,1) + (partb.getFloat(it,ipro)-mcb.getFloat(it,1))*mfac)})
+
+			    def evec = new Vector3()
+			    evec.setMagThetaPhi(ele.p(), ele.theta(), ele.phi())
+			    def pvec = new Vector3()
+			    pvec.setMagThetaPhi(pro.p(), pro.theta(), pro.phi())
+			  }
+
+			  def wvec = beam+target-ele
+			  def qvec = beam-ele
+			  def epx = beam+target-ele-pro
+
+			  def pdet = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 'FD':'CD'
+
+			  def profi = Math.toDegrees(pro.phi())
+			  if(profi<0) profi+=360
+
+			  def esec = (0..<scib.rows()).find{scib.getShort('pindex',it)==iele}?.with{scib.getByte('sector',it)}
+			  def psec = (0..<scib.rows()).find{scib.getShort('pindex',it)==ipro}?.with{scib.getByte('sector',it)}
+			  if(psec==0) {
+			    psec = Math.floor(profi/60).toInteger() +2
+			    if(psec==7) psec=1
+			  }
+
+			  def isep0 = epx.mass2()<1 && wvec.mass()>2
+
+			  return isep0
+			}
+			print "isep0s is" + isep0s
 			return ihel
 
 
