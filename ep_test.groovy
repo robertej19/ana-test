0bb2938113f3d0e111e911c1544cf80713952fe0
robertej19 <robertej@mit.edu>
2020-02-10 13:40:40 -0500

    ""

--- ep_test.groovy
+++ ep_test.groovy
@@ -22,13 +22,6 @@ class ep_test {
 
   def hw = {new H1F("$it","$it",200,0,5)}
   def hq2 = {new H1F("$it","$it",200,0,10)}
-  def hfi = {new H1F("$it","$it",200,-360,360)}
-
-  def hepx0 = {new H1F("$it","$it",200,-1,2)}
-  def hepx = {new H1F("$it","$it",200,-0.7,0.7)}
-  def hepxmom = {new H2F("$it","$it",200,0,4,200,-0.7,0.7)}
-  def hepxth = {new H2F("$it","$it",300,0,60,200,-0.7,0.7)}
-
 
    def banknames = ['REC::Event','REC::Particle','REC::Cherenkov','REC::Calorimeter','REC::Traj','REC::Track','REC::Scintillator']
   def processEvent(event) {
@@ -50,81 +43,9 @@ class ep_test {
             .collect{ig2->[ig1,ig2]}
         }
 
-      def isep0s = ieps.findAll{iele,ipro->
-        def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
-        def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
-
-        if(event.hasBank("MC::Particle")) {
-          def mcb = event.getBank("MC::Particle")
-          def mfac = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 3.2 : 2.5
-
-          def profac = 0.9
-
-          //mfac=1
-          profac = 1.0
-
-          ele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0) + (partb.getFloat(it,iele)-mcb.getFloat(it,0))*mfac})
-          pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{profac*(mcb.getFloat(it,1) + (partb.getFloat(it,ipro)-mcb.getFloat(it,1))*mfac)})
-
-	println "electron (line 60) is " + ele
-	println "proton (line 60) is " + pro
-
-          def evec = new Vector3()
-          evec.setMagThetaPhi(ele.p(), ele.theta(), ele.phi())
-          def pvec = new Vector3()
-          pvec.setMagThetaPhi(pro.p(), pro.theta(), pro.phi())
-        }
-
-        def wvec = beam+target-ele
-        def qvec = beam-ele
-        def epx = beam+target-ele-pro
-
-        println "wvec is " + wvec
-        println "qvec " + qvec
-
-        def pdet = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 'FD':'CD'
-
-        def profi = Math.toDegrees(pro.phi())
-        if(profi<0) profi+=360
-
-        def esec = (0..<scib.rows()).find{scib.getShort('pindex',it)==iele}?.with{scib.getByte('sector',it)}
-        def psec = (0..<scib.rows()).find{scib.getShort('pindex',it)==ipro}?.with{scib.getByte('sector',it)}
-        if(psec==0) {
-          psec = Math.floor(profi/60).toInteger() +2
-          if(psec==7) psec=1
-        }
-        println "esec " + esec
-        println "psec " + psec
-
-        def pi0s = ipi0s.collect{ig1,ig2->
-          def g1 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig1)})
-          def g2 = LorentzVector.withPID(22,*['px','py','pz'].collect{partb.getFloat(it,ig2)})
-          if(ele.vect().theta(g1.vect())>8 && ele.vect().theta(g2.vect())>8) {
-            def gg = g1+g2
-            def ggmass = gg.mass()
-            def ispi0 = ggmass<0.2 && ggmass>0.07// && gg.p()>1.5
-
-            if(ispi0) {
-              def epggx = epx-gg
-              def thetaXPi = epx.vect().theta(gg.vect())
-
-              def dpt0 = epggx.px().abs()<0.3 && epggx.py().abs()<0.3
-              def dmisse0 = epggx.e()<1
-
-              def tt0 = -(pro-target).mass2()
-              def procalc = beam+target-ele-gg
-              def tt1 = -(procalc-target).mass2()
-
-              }
-              return ispi0
-            }
-          }
-          return false
+      println "ihel is "+ihel
+      println "ieps is "+ieps
+      println "ipi0s is " +ipi0s
+}
+}
         
-
-
-      // def goodgammas = ele.vect().theta(g1.vect())>8 && ele.vect().theta(g2.vect())>8
-      // def prelims = goodgammas && epx.mass2()<1 && g1stat>=2000 && g2stat>=2000 && fgg>0.05 && fgg<0.3 && epggx.e()<1 && ftheta<16
-      // def finals = prelims && ftheta<2 && fmisse<0.9 && epx.mass2().abs()<0.7
-    }
-  }
