1ee871aec81993fc46eaadb94aa3889f9795ec62
robertej19 <robertej@mit.edu>
2020-02-11 10:17:08 -0500

    ""

--- prime_sector.groovy
+++ prime_sector.groovy
@@ -52,79 +52,101 @@ def processEvent(event,hhel,hphi,hq2,hW) {
 
 	def banknames = ['REC::Event','REC::Particle','REC::Cherenkov','REC::Calorimeter','REC::Traj','REC::Track','REC::Scintillator']
 
-	    if(banknames.every{event.hasBank(it)}) {
-		    def (evb,partb,cc,ec,traj,trck,scib) = banknames.collect{event.getBank(it)}
-		    def banks = [cc:cc,ec:ec,part:partb,traj:traj,trck:trck]
-		    def ihel = evb.getByte('helicity',0)
-		   //println "ihel is "+ihel
+	if(banknames.every{event.hasBank(it)}) {
+		def (evb,partb,cc,ec,traj,trck,scib) = banknames.collect{event.getBank(it)}
+		def banks = [cc:cc,ec:ec,part:partb,traj:traj,trck:trck]
+		def ihel = evb.getByte('helicity',0)
+		//println "ihel is "+ihel
 
 
 
-		   def index_of_electrons_and_protons = (0..<partb.rows()).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0}
-			   .collectMany{iele->(0..<partb.rows()).findAll{partb.getInt('pid',it)==2212}.collect{ipro->[iele,ipro]}
-		   }
-		   //println "index_of_electrons_and_protons "+index_of_electrons_and_protons
+		def index_of_electrons_and_protons = (0..<partb.rows()).findAll{partb.getInt('pid',it)==11 && partb.getShort('status',it)<0}
+			.collectMany{iele->(0..<partb.rows()).findAll{partb.getInt('pid',it)==2212}.collect{ipro->[iele,ipro]}
+		}
+		//println "index_of_electrons_and_protons "+index_of_electrons_and_protons
 
-		   def index_of_pions = (0..<partb.rows()-1).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
-			   .findAll{ig1->'xyz'.collect{partb.getFloat("p$it",ig1)**2}.sum()>0.16}
-			   .collectMany{ig1->
-			   (ig1+1..<partb.rows()).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
-			   .findAll{ig2->'xyz'.collect{partb.getFloat("p$it",ig2)**2}.sum()>0.16}
-			   .collect{ig2->[ig1,ig2]}
-		   }
-		   //println "index of pions is " + index_of_pions
+		def index_of_pions = (0..<partb.rows()-1).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
+			.findAll{ig1->'xyz'.collect{partb.getFloat("p$it",ig1)**2}.sum()>0.16}
+			.collectMany{ig1->
+			(ig1+1..<partb.rows()).findAll{partb.getInt('pid',it)==22 && partb.getShort('status',it)>=2000}
+			.findAll{ig2->'xyz'.collect{partb.getFloat("p$it",ig2)**2}.sum()>0.16}
+			.collect{ig2->[ig1,ig2]}
+		}
+		//println "index of pions is " + index_of_pions
 
-		   def isep0s = index_of_electrons_and_protons.findAll{iele,ipro->
-			   def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
-			   def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
-			   //println "first electron is"+ele
-			   if(event.hasBank("MC::Particle")) {
-				   //println "Event has MC Particle bank!"
-				   def mcb = event.getBank("MC::Particle")
-				   def mfac = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 3.2 : 2.5
+		def isep0s = index_of_electrons_and_protons.findAll{iele,ipro->
+			def ele = LorentzVector.withPID(11,*['px','py','pz'].collect{partb.getFloat(it,iele)})
+			def pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{partb.getFloat(it,ipro)})
+			//println "first electron is"+ele
+			if(event.hasBank("MC::Particle")) {
+				//println "Event has MC Particle bank!"
+				def mcb = event.getBank("MC::Particle")
+				def mfac = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 3.2 : 2.5
 
-				   def profac = 0.9
+				def profac = 0.9
 
-				   //mfac=1
-				   profac = 1.0
+				//mfac=1
+				profac = 1.0
 
-				   ele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0) + (partb.getFloat(it,iele)-mcb.getFloat(it,0))*mfac})
-				   pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{profac*(mcb.getFloat(it,1) + (partb.getFloat(it,ipro)-mcb.getFloat(it,1))*mfac)})
-				   //println "second electron is"+ele
-				   def evec = new Vector3()
-				   evec.setMagThetaPhi(ele.p(), ele.theta(), ele.phi())
-				   def pvec = new Vector3()
-				   pvec.setMagThetaPhi(pro.p(), pro.theta(), pro.phi())
-			   }
+				ele = LorentzVector.withPID(11,*['px','py','pz'].collect{mcb.getFloat(it,0) + (partb.getFloat(it,iele)-mcb.getFloat(it,0))*mfac})
+				pro = LorentzVector.withPID(2212,*['px','py','pz'].collect{profac*(mcb.getFloat(it,1) + (partb.getFloat(it,ipro)-mcb.getFloat(it,1))*mfac)})
+				//println "second electron is"+ele
+				def evec = new Vector3()
+				evec.setMagThetaPhi(ele.p(), ele.theta(), ele.phi())
+				def pvec = new Vector3()
+				pvec.setMagThetaPhi(pro.p(), pro.theta(), pro.phi())
+			}
 
-			   def wvec = beam+target-ele
-			   def qvec = beam-ele
-			   def epx = beam+target-ele-pro
+			def wvec = beam+target-ele
+			def qvec = beam-ele
+			def epx = beam+target-ele-pro
 
-			   //println "qvec is " + qvec
-			   //println "qvec comp is " + -qvec.mass2()
+			//println "qvec is " + qvec
+			//println "qvec comp is " + -qvec.mass2()
 
-			   def pdet = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 'FD':'CD'
+			def pdet = (partb.getShort('status',ipro)/1000).toInteger()==2 ? 'FD':'CD'
 
-			   def profi = Math.toDegrees(pro.phi())
-			   if(profi<0) profi+=360
+			def profi = Math.toDegrees(pro.phi())
+			if(profi<0) profi+=360
 
-			   hhel.fill(ihel)
-			   hphi.fill(profi)
-			   hq2.fill(-qvec.mass2())
-			   hW.fill(wvec.mass())
+			def esec = (0..<scib.rows()).find{scib.getShort('pindex',it)==iele}?.with{scib.getByte('sector',it)}
+			def psec = (0..<scib.rows()).find{scib.getShort('pindex',it)==ipro}?.with{scib.getByte('sector',it)}
+			if(psec==0) {
+				psec = Math.floor(profi/60).toInteger() +2
+				if(psec==7) psec=1
+			}
 
-		   }
 
 
+
+
+			hhel.fill(ihel)
+			hphi.fill(profi)
+			hq2.fill(-qvec.mass2())
+			hW.fill(wvec.mass())
+
+		 }
 	}
-
 }
+
+
+def total_events = new AtomicInteger()
+while(reader.hasEvent()) {
+	total_events.getAndIncrement()
+	println "Counting number of events in file"
+	if(total_events.get() % 100000 == 0){
+	  println "event count: "+total_events.get()/100000 + "00 K"
+  }
+
+println ""
+println "Total number of events is " + total_events.get()/10000 + "0 K"
+println ""
+
 
 def smalltest = 0
 
 if (smalltest == 0){
-	println "smalltest is zero"
+	println "Processing entire datafile"
 	def evcount = new AtomicInteger()
 	evcount.set(0)
 	while(reader.hasEvent()) {
