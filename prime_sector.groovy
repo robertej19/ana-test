6aeca568c69f594af40671a9ec02dab04510021a
robertej19 <robertej@mit.edu>
2020-02-12 16:57:30 -0500

    ""

--- prime_sector.groovy
+++ prime_sector.groovy
@@ -39,14 +39,14 @@ println("\n \n \n \n \n \n \n \n \n \n \n \n \n \n")
 def printer(string,override){
 	k = 0
 	if(k==1){
-		println(string+"\n")
+		println("\n"+string+"\n")
 		if(override==2){
 			println(string)
 		}
 	}
 	if(k==0){
 		if(override==1){
-			println(string+"\n")
+			println("\n"+string+"\n")
 		}
 		if(override==2){
 			println(string)
