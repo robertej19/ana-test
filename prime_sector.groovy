b3193d2e1964fa665b7324d36527753b2407bfe3
robertej19 <robertej@mit.edu>
2020-02-14 10:38:11 -0500

    ""

--- prime_sector.groovy
+++ prime_sector.groovy
@@ -29,6 +29,9 @@ import org.jlab.io.base.DataEvent
 import org.jlab.io.hipo.HipoDataSource
 import org.jlab.io.hipo.HipoDataSync
 
+GStyle.getAxisAttributesX().setTitleFontSize(98);
+GStyle.getAxisAttributesX().setLabelFontSize(90);
+
 
 MyMods.enable()
 
@@ -241,8 +244,6 @@ printer("Done processing data",1)
 
 reader.close()
 
-GStyle.getAxisAttributesX().setTitleFontSize(98);
-GStyle.getAxisAttributesX().setLabelFontSize(90);
 
 def run = "testrun5036"
 TDirectory out = new TDirectory()
