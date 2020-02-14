acb2d2a16fb56a2a8dadd1e24cbe13649be4e4f1
robertej19 <robertej@mit.edu>
2020-02-14 10:53:44 -0500

    ""

---
+++ test.groovy
@@ -0,0 +1,24 @@
+import java.util.Random;
+import javax.swing.JFrame;
+
+import org.jlab.groot.data.H1F;
+import org.jlab.groot.graphics.EmbeddedCanvas;
+
+public class BasicDemo {
+
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Basic GROOT Demo");
+		EmbeddedCanvas canvas = new EmbeddedCanvas();
+		frame.setSize(800,500);
+		H1F histogram = new H1F("histogram",100,-5,5);
+		Random randomGenerator = new Random();
+		for(int i=0; i<5000; i++){
+			histogram.fill(randomGenerator.nextGaussian());
+		}
+		canvas.draw(histogram);
+		frame.add(canvas);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+}
