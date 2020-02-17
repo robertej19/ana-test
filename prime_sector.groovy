f26d23bfbf2059c8afe75dae8c6e10a19278ccaf
robertej19 <robertej@mit.edu>
2020-02-17 11:44:40 -0500

    ""

--- prime_sector.groovy
+++ prime_sector.groovy
@@ -200,10 +200,6 @@ def screen_updater(FileStartTime,CurrentCounter,CountRate,NumTotalCounts){
 	}
 }
 
-
-
-
-
 def reader = new HipoDataSource()
 if (args.size()<2) {
 	printer("You need to include the number of events you want to process in the start command!",1)
@@ -220,6 +216,7 @@ for (FileName in FilesToProcess){
 }
 
 for (int i=0; i < FilesToProcess.size(); i++) {
+	println "FILE INDEX NUMBER IS $i"
 	def fname = FilesToProcess[i]
 	reader.open(fname)
 	def NumEventsInFile= reader.getSize().toInteger()
